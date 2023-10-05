51da7bb261f0ad5261c762f98aa142eb41666913
Peter Oberlaender <pete@wayoutofline.net>
2023-10-05 14:50:18 -0400

    Added random number function & feature...

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # catch-that-number-game
 random JS number catcher game...
 
-- When you hit Enter while counter is 10, 
+- When you hit Enter when counter reaches a certain random number:
 - interrupt & reset to 0 of the counter
 - alert("YAY! You stopped the counter 🥳"); will interrupt the counting automatically
 
 ## Todo:
-- randomize Number each time...
 - style this properly! LOL
--- js/game.js
+++ js/game.js
@@ -1,29 +1,29 @@
 const numberContainer = document.querySelector("#counter");
+const catchNumberDisplay = document.querySelector("#number");
 let counter = 0;
+let catchNumber;
+
+const generateRandomNumber =  function() {
+    catchNumber = Math.floor((Math.random() * 50) + 1);
+    catchNumberDisplay.innerHTML = catchNumber;
+}
+generateRandomNumber();
 
 //counting mechanism
 setInterval(function(){
     // task 1
     // increase counter by 1 each time
     
-    
-    //counter += 1;
-    //counter = counter + 1;
-    
     counter++;
     numberContainer.innerHTML = counter;
-    
-    
-    console.log(counter);
+    // console.log(counter);
 
     // task 2 | conditional 1
     // reset the counter to 0 when it reaches 50
     if (counter == 50) {
         counter = 0;
     }
-    
-    
-}, 200);
+}, 100);
 
 // Hit Enter - Event
 window.addEventListener("keyup", function(e) {
@@ -31,11 +31,11 @@ window.addEventListener("keyup", function(e) {
         
         //alert("booh");
 
-        if (counter == 10) {
-            
+        if (counter == catchNumber) {
             console.log(counter);
             alert("YAY! You stopped the counter 🥳");
-            counter = -1;
+            counter -= 1; //fix number display
+            generateRandomNumber();
         }
         // task 3 | conditional 2
         // When you hit Enter while counter is 10, 
